e87dd9d181278b6f0648d03b643cf084a3f695e4
Radek <radoslaw.parol95@gmail.com>
2024-09-12 21:16:41 +0200

    add builder

--- src/main/java/org/example/domain/user/UserDto.java
+++ src/main/java/org/example/domain/user/UserDto.java
@@ -1,10 +1,14 @@
 package org.example.domain.user;
 
+import lombok.Builder;
+
+@Builder
 public record UserDto(
-   long id,
-   String name,
-   String surname,
-   String email,
-   String username,
-   String password
-) {}
+        long id,
+        String name,
+        String surname,
+        String email,
+        String username,
+        String password
+) {
+}
